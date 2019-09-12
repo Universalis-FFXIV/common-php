495d1b01a4a70546dea04ee453a1931652d3d823
Jonas <fakecake@icloud.com>
2019-09-12 19:16:00 +0900

    Change GameServers to use real World ids

--- Game/GameServers.php
+++ Game/GameServers.php
@@ -4,91 +4,11 @@ namespace App\Common\Game;
 
 use App\Common\Exceptions\CompanionMarketServerException;
 use Delight\Cookie\Cookie;
+use App\Common\Service\Redis\Redis;
 
 class GameServers
 {
     const DEFAULT_SERVER = 'Phoenix';
-    
-    const MARKET_OFFLINE = [
-        // JP Servers
-        1,2,3,4,5,6,9,12,14,17,22,23,26,27,29,30,32,38,39,45,48,49,51,54,55,56,57,58,60,61,62,64
-    ];
-
-    /**
-     * It is important new servers are added to the end of this list and that the order is NEVER changed.
-     */
-    const LIST = [
-        'Adamantoise',  # 0
-        'Aegis',        # 1
-        'Alexander',    # 2
-        'Anima',        # 3
-        'Asura',        # 4
-        'Atomos',       # 5
-        'Bahamut',      # 6
-        'Balmung',      # 7
-        'Behemoth',     # 8
-        'Belias',       # 9
-        'Brynhildr',    # 10
-        'Cactuar',      # 11
-        'Carbuncle',    # 12
-        'Cerberus',     # 13
-        'Chocobo',      # 14
-        'Coeurl',       # 15
-        'Diabolos',     # 16
-        'Durandal',     # 17
-        'Excalibur',    # 18
-        'Exodus',       # 19
-        'Faerie',       # 20
-        'Famfrit',      # 21
-        'Fenrir',       # 22
-        'Garuda',       # 23
-        'Gilgamesh',    # 24
-        'Goblin',       # 25
-        'Gungnir',      # 26
-        'Hades',        # 27
-        'Hyperion',     # 28
-        'Ifrit',        # 29
-        'Ixion',        # 30
-        'Jenova',       # 31
-        'Kujata',       # 32
-        'Lamia',        # 33
-        'Leviathan',    # 34
-        'Lich',         # 35
-        'Louisoix',     # 36
-        'Malboro',      # 37
-        'Mandragora',   # 38
-        'Masamune',     # 39
-        'Mateus',       # 40
-        'Midgardsormr', # 41
-        'Moogle',       # 42
-        'Odin',         # 43
-        'Omega',        # 44
-        'Pandaemonium', # 45
-        'Phoenix',      # 46
-        'Ragnarok',     # 47
-        'Ramuh',        # 48
-        'Ridill',       # 49
-        'Sargatanas',   # 50
-        'Shinryu',      # 51
-        'Shiva',        # 52
-        'Siren',        # 53
-        'Tiamat',       # 54
-        'Titan',        # 55
-        'Tonberry',     # 56
-        'Typhon',       # 57
-        'Ultima',       # 58
-        'Ultros',       # 59
-        'Unicorn',      # 60
-        'Valefor',      # 61
-        'Yojimbo',      # 62
-        'Zalera',       # 63
-        'Zeromus',      # 64
-        'Zodiark',      # 65
-
-        // New for 5.0
-        'Spriggan',     # 66
-        'Twintania',    # 67
-    ];
     
     const MARKET_SERVER = 'Balmung';
 
@@ -204,7 +124,8 @@ class GameServers
     public static function getServer(string $pvodied = null): string
     {
         $server = ucwords($pvodied ?: Cookie::get('mogboard_server'));
-        return in_array($server, self::LIST) ? $server : self::DEFAULT_SERVER;
+        $worldMap = (Array) Redis::Cache()->get('xiv_World_Map');
+        return in_array($server, $worldMap) ? $server : self::DEFAULT_SERVER;
     }
 
     /**
@@ -212,7 +133,8 @@ class GameServers
      */
     public static function getServerId(string $server): int
     {
-        $index = array_search(ucwords($server), GameServers::LIST);
+        $worldMap = (Array) Redis::Cache()->get('xiv_World_Map');
+        $index = array_search(ucwords($server), $worldMap);
 
         if ($index === false) {
             throw new CompanionMarketServerException();
